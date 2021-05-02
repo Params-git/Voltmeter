fd5c16a0665588c22ae7f6a2147cfa739ff7752b
[Params-git] <singhparamjeet9250@gmail.com>
2021-05-03 01:29:44 +0530

    update

--- 3_phase_voltmeter/3_phase_voltmeter/main.c
+++ 3_phase_voltmeter/3_phase_voltmeter/main.c
@@ -5,40 +5,26 @@
  * Author : Paramjeet singh
  */ 
 
-#include <avr/io.h>
-#define F_CPU 8000000UL
-#include <util/delay.h>
-#include <stdlib.h>
-#include "lcd-routines.h"
-
 #ifndef F_CPU
 #define F_CPU 8000000UL
 #endif
 
-/*int adc_read(int ch);
-int adc_read2(int ch);
-int adc_read3(int ch);*/
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include "lcd-routines.h"
 
 int adc;
-int adc2;
-int adc3;
-
 char buf[20];
-char buf2[20];
-char buf3[20];
-
 float vm = 0;
-float vm2 = 0;
-float vm3 = 0;
 
 void adc_init()
 {
-	// AREF = AVcc
+	//AREF = AVcc
 	ADMUX = (1<<REFS0); //for enabling reference voltage 
 	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);  //ADC enable and set prescaler
 }
 
-// read adc value
 uint16_t adc_read(uint8_t ch){
 	
 	ch &= 0b00000111; 
@@ -49,93 +35,88 @@ uint16_t adc_read(uint8_t ch){
 	return (ADCW);
 }
 
-int main(void)
+
+int adc_phase(int ch)
 {
-	uint16_t adc_int[41];
-	uint16_t adc_int2[41];
-	uint16_t adc_int3[41];
-	int i = 0, Max = 0, Max2 = 0, Max3 = 0;
+	uint16_t adc_int[100];
+	int i = 0, Max = 0;
 	
+	for (i = 0; i <= 79; i++) {
+		adc_int[i] = adc_read(ch);      // read adc value at PORTC.0
+	}
+	
+	Max = adc_int[0];
+	
+	for (i = 0; i <= 80; i++) {
+		if (Max < adc_int[i])
+		Max = adc_int[i];
+	}
+	
+	adc = Max;
+	itoa(Max, buf, 10);
+	
+	_delay_ms(1000);
+	
+	vm = (float)(adc * 0.2763637188); // 283/1024 = 0.2763637188
+	adc = (int)vm;
+	
+	itoa(adc, buf, 10);
+	
+	return *buf;
+	
+}
+
+
+int main(void)
+{	
 	lcd_init();
 	lcd_setcursor(0, 1);
 	lcd_string("AC VOLTMETER..");
-	_delay_ms(1000);
+	_delay_ms(500);
 	lcd_clear();
-	lcd_string("intialising...");
-	
+	lcd_string("intialising ADC..");
 	
 	adc_init();
 	
 	while(1){
 		
-		_delay_ms(100);
-
-		for (i = 0; i <= 39; i++) {
-			adc_int[i] = adc_read(0);      // read adc value at PORTC.0
-			adc_int2[i] = adc_read(1);
-			adc_int3[i] = adc_read(2);
-		}
-
-		
-		Max = adc_int[0];
-		Max2 = adc_int2[0];
-		Max3 = adc_int3[0];
-		
-		for (i = 0; i <= 40; i++) {
-			if (Max < adc_int[i])
-			Max = adc_int[i];
+		for(int i=0; i<=2; i++)
+		{
+			adc_phase(i);  //Phase1, Phase2, Phase3
+			/*_delay_ms(1000);*/
 			
-			if (Max2 < adc_int2[i])
-			Max2 = adc_int2[i];
+			if(i==0)
+			{
+				lcd_clear();
+				lcd_setcursor(0,1);
+				lcd_string("Phase1: ");
+				lcd_setcursor(0,8);
+				lcd_string((const char*)buf);
+			}	
 			
-			if (Max3 < adc_int3[i])
-			Max3 = adc_int3[i];
+			else if(i==1)
+			{
+				lcd_clear();
+				lcd_setcursor(0,1);
+				lcd_string("Phase2: ");
+				lcd_setcursor(0,8);
+				lcd_string((const char*)buf);
+			}
+			
+			else if(i==2)
+			{
+				lcd_clear();
+				lcd_setcursor(0,1);
+				lcd_string("Phase3: ");
+				lcd_setcursor(0,8);
+				lcd_string((const char*)buf);
+			}
+			
+			else
+			{
+				return 0;
+			}
+			/*_delay_ms(500);*/
 		}
-
-		adc = Max;
-		adc2 = Max2;
-		adc3 = Max3;
-		
-		itoa(Max, buf, 10);
-		itoa(Max2, buf2, 10);
-		itoa(Max3, buf3, 10);
-		
-		_delay_ms(1000);
-		
-		vm = (float)(adc * 0.27734375); // 283/1024 = 0.2763637188
-		vm2 = (float)(adc2 * 0.27734375); // 283/1024 = 0.2763637188
-		vm3 = (float)(adc3 * 0.27734375); // 283/1024 = 0.2763637188
-		
-		adc = (int)vm;
-		adc2 = (int)vm2;
-		adc3 = (int)vm3;
-		
-		itoa(adc, buf, 10);
-		itoa(adc2, buf2, 10);
-		itoa(adc3, buf3, 10);
-		
-		_delay_ms(1000);
-		
-		lcd_clear();
-		lcd_setcursor(0,1);
-		lcd_string("Phase1: ");
-		lcd_setcursor(0,8);
-		lcd_string((const char*)buf);
-		_delay_ms(1000);
-		
-		lcd_clear();
-		lcd_setcursor(0,1);
-		lcd_string("Phase2: ");
-		lcd_setcursor(0,8);
-		lcd_string((const char*)buf2);
-		_delay_ms(1000);
-		
-		lcd_clear();
-		lcd_setcursor(0,1);
-		lcd_string("Phase3: ");
-		lcd_setcursor(0,8);
-		lcd_string((const char*)buf3);
-		_delay_ms(1000);
 	}
-	return 0;
 }
